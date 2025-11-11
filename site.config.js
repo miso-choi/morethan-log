7413acf23229d14d60ba3096d66a2a3b6c20df1a
Miso CHOI <miso8070@korea.ac.kr>
2025-11-12 03:04:35 +0900

    fix code issue
    Updated projects section to include a placeholder for project name and link.

--- site.config.js
+++ site.config.js
@@ -10,12 +10,12 @@ const CONFIG = {
     github: "https://github.com/miso-choi",
     instagram: "",
   },
-  // projects: [
-  //   {
-  //     name: `morethan-log`,
-  //     href: "https://github.com/morethanmin/morethan-log",
-  //   },
-  // ],
+  projects: [
+    {
+      name: ``, // morethan-log
+      href: "", // https://github.com/morethanmin/morethan-log
+    },
+  ],
   // blog setting (required)
   blog: {
     title: "morethan-log",
